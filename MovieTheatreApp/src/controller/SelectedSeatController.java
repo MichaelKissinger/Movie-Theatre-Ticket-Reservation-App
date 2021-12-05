1c1f35e0099f7330f340d82ce75e226e7314d78a
Jared Kraus <jared.kraus3@gmail.com>
2021-12-04 22:25:22 -0700

    update selected seat view

--- MovieTheatreApp/src/controller/SelectedSeatController.java
+++ MovieTheatreApp/src/controller/SelectedSeatController.java
@@ -5,11 +5,13 @@ import model.User;
 import view.SelectSeatView;
 
 import javax.swing.*;
+import java.util.ArrayList;
 
 public class SelectedSeatController {
 
     private User user;
     private Showing showing;
+    private ArrayList<String> purchasedSeats;
 
     public SelectedSeatController(User user, Showing showing) {
 
@@ -20,7 +22,7 @@ public class SelectedSeatController {
         selectSeatView.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
         selectSeatView.addFinishedButtonListener(e -> {
-
+            purchasedSeats = selectSeatView.getSelectedSeats();
         });
 
     }
