3dd7edb6934f0988602053355065f41d14f35f18
Nicolas L'Amarca <nicolas.lamarca@ucalgary.ca>
2021-12-05 21:07:26 -0700

    Email model class

---
+++ MovieTheatreApp/src/model/Email.java
@@ -0,0 +1,60 @@
+package model;
+
+import java.util.Date;
+
+public class Email {
+
+    private int emailId;
+    private int userId;
+    private String message;
+    private Date sentDate;
+    private String subject;
+
+    public Email(String message, String subject, Date sentDate)
+    {
+        setMessage(message);
+        setSubject(subject);
+        setSentDate(sentDate);
+
+    }
+
+    public int getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(int emailId) {
+        this.emailId = emailId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Date getSentDate() {
+        return sentDate;
+    }
+
+    public void setSentDate(Date sentDate) {
+        this.sentDate = sentDate;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+}
