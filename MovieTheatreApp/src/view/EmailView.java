ee01d6c43f2d6c6794b57898383a9989c65e8f6f
Nicolas L'Amarca <nicolas.lamarca@ucalgary.ca>
2021-12-05 21:23:22 -0700

    Email View Dev started.

---
+++ MovieTheatreApp/src/view/EmailView.java
@@ -0,0 +1,13 @@
+package view;
+
+import java.awt.event.ActionListener;
+
+public class EmailView {
+
+
+    public void addViewEmailButtonListener(ActionListener listener) {
+        viewEmailButton.addActionListener(listener);
+    }
+
+}
+}
