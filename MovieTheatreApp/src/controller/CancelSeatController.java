092064ab4dd2e1fb2db2174fe7eb2f629e5ba5ef
YKou-Kody <83717117+YKou-Kody@users.noreply.github.com>
2021-12-06 16:45:20 -0700

    added views and controllers for ticket cancellation process, not entirely functioning yet

---
+++ MovieTheatreApp/src/controller/CancelSeatController.java
@@ -0,0 +1,41 @@
+package controller;
+
+import model.Seat;
+import model.Transaction;
+import model.User;
+import view.CancelSeatView;
+
+import javax.swing.*;
+
+public class CancelSeatController {
+    private User user;
+    Transaction transaction;
+    Seat seat;
+
+    public CancelSeatController(User user, int index){
+        CancelSeatView cancelSeatView =
+                new CancelSeatView(user.getPreviousPurchases().get(index).getPurchasedSeats());
+        cancelSeatView.setVisible(true);
+
+        cancelSeatView.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        cancelSeatView.addCancelButtonListener(e->{
+            int seatIndex = cancelSeatView.getListIndex();
+            seat = user.getPreviousPurchases().get(index).getPurchasedSeats().get(seatIndex);
+            //TODO search the seat in the database,
+
+        });
+
+        cancelSeatView.addBackButtonListener(e->{
+            //TODO implement terminalview here
+        });
+
+
+
+
+
+    }
+
+
+
+}
